ce9d11a258dc3a71b70158473b25e43526b731f7
StasRivkin <rsl14591@gmail.com>
2024-11-16 20:57:33 +0200

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,48 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatTabsModule} from "@angular/material/tabs";
+import { TabsComponent } from './components/tabs/tabs.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TabBodyComponent } from './components/tab-body/tab-body.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatPaginatorModule} from "@angular/material/paginator";
+import {MatListModule} from "@angular/material/list";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    HeaderComponent,
+    TabBodyComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    MatToolbarModule,
+    MatButtonModule,
+    MatTabsModule,
+    BrowserModule,
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatPaginatorModule,
+    MatListModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
