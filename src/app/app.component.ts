3fc5c082c6fc1705131a6bb6e346cd5f03b1d888
StasRivkin <rsl14591@gmail.com>
2024-11-18 11:22:20 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,23 @@
 import { Component } from '@angular/core';
-import {SwUpdate} from "@angular/service-worker";
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'pimsler-hebrew';
 
   constructor(private swUpdate: SwUpdate) {
-    // if (this.swUpdate.isEnabled) {
-    //   this.swUpdate.versionUpdates.subscribe(() => {
-    //     if (confirm("New version available. Load New Version?")) {
-    //       window.location.reload();
-    //     }
-    //   });
-    // }
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.versionUpdates.subscribe(event => {
+        if (event.type === 'VERSION_READY') {
+          if (confirm('A new version of the app is available. Would you like to load the new version?')) {
+            window.location.reload();
+          }
+        }
+      });
+    }
   }
 }
