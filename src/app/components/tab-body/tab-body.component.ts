ec1f2a1a164708276da5e6c133f479842fff4d97
StasRivkin <rsl14591@gmail.com>
2024-11-19 10:10:20 +0200

    init

--- src/app/components/tab-body/tab-body.component.ts
+++ src/app/components/tab-body/tab-body.component.ts
@@ -1,102 +1,73 @@
-import {AfterViewInit, Component, ElementRef, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
-import {PageEvent} from "@angular/material/paginator";
-import {DataStoreService} from "../../store/data-store.service";
-import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
+import { Component, OnInit } from '@angular/core';
+import { AudioLoaderService } from '../../services/audio-loader.service';
+import { DataStoreService } from "../../store/data-store.service";
 
 @Component({
   selector: 'app-tab-body',
   templateUrl: './tab-body.component.html',
-  styleUrl: './tab-body.component.css'
+  styleUrls: ['./tab-body.component.css']
 })
 export class TabBodyComponent implements OnInit {
   audios: any[] = [];
-  pageSize = 6;
-  currentPage = 0;
   paginatedAudios: any[] = [];
+  currentPage = 0;
+  pageSize = 6;
 
   currentAudio: HTMLAudioElement | undefined;
   isPlaying = false;
 
-  constructor(private store: DataStoreService) {
-  }
+  constructor(
+    private audioLoaderService: AudioLoaderService,
+    private store: DataStoreService
+  ) {}
 
   ngOnInit(): void {
-    this.store.getCurPart().subscribe(curPart => {
-      this.updateAudios(curPart);
+    this.loadAudios(1);
+    this.store.getAudios().subscribe(audios => {
+      this.audios = audios;
       this.updatePaginatedAudios();
     });
-    this.store.getCurAudio().subscribe(curAudio => {
-      if (curAudio) {
-        this.stopAllAudios(curAudio);
-        this.currentAudio = curAudio;
-        this.isPlaying = !curAudio.paused;
-        this.currentAudio.play();
-      }else{
-        this.currentAudio?.pause();
-      }
-    });
   }
 
-  onPlay(currentAudio: HTMLAudioElement) {
-    this.store.setCurAudio(currentAudio);
+  loadAudios(part: number): void {
+    this.audioLoaderService.preloadAudioFiles(part);
   }
 
-  onPause(currentAudio: HTMLAudioElement) {
+  onPlay(audio: any, audioPlayer: HTMLAudioElement): void {
+    if (this.currentAudio && this.currentAudio !== audioPlayer) {
+      this.stopAllAudios(this.currentAudio);
+    }
+    audioPlayer.src = audio.url;
+    audioPlayer.load();
+    audioPlayer.play();
+    this.store.setCurAudio(audioPlayer);
+  }
+
+  onPause(currentAudio: HTMLAudioElement): void {
     if (currentAudio) {
       currentAudio.pause();
       this.isPlaying = false;
     }
   }
 
-  onPageChange(event: PageEvent) {
+  onPageChange(event: any): void {
     this.currentPage = event.pageIndex;
     this.pageSize = event.pageSize;
     this.updatePaginatedAudios();
   }
 
-  private updateAudios(curPart: number) {
-    switch (curPart) {
-      case 0:
-        this.audios = Array.from({length: 30}, (_, i) => ({
-          title: `Урок ${i + 1}`,
-          description: `Описание урока ${i + 1}`,
-          url: `/assets/part1/part1-lesson-${i + 1}.mp3`
-        }));
-        break;
-      case 1:
-        this.audios = Array.from({length: 30}, (_, i) => ({
-          title: `Урок ${i + 1}`,
-          description: `Описание урока ${i + 1}`,
-          url: `/assets/part2/part2-lesson-${i + 1}.mp3`
-        }));
-        break;
-      case 2:
-        this.audios = Array.from({length: 30}, (_, i) => ({
-          title: `Урок ${i + 1}`,
-          description: `Описание урока ${i + 1}`,
-          url: `/assets/part3/part3-lesson-${i + 1}.mp3`
-        }));
-        break;
-      default:
-        this.audios = [];
-    }
-  }
-
-  private updatePaginatedAudios() {
+  private updatePaginatedAudios(): void {
     const startIndex = this.currentPage * this.pageSize;
     const endIndex = startIndex + this.pageSize;
     this.paginatedAudios = this.audios.slice(startIndex, endIndex);
   }
 
   private stopAllAudios(currentAudio: HTMLAudioElement): void {
-    const sourceElement = currentAudio.querySelector('source');
-    if (sourceElement) {
-      const allAudioPlayers = document.querySelectorAll('audio');
-      allAudioPlayers.forEach((audio: HTMLAudioElement) => {
-        if (audio !== currentAudio) {
-          audio.pause();
-        }
-      });
-    }
+    const allAudioPlayers = document.querySelectorAll('audio');
+    allAudioPlayers.forEach((audio: HTMLAudioElement) => {
+      if (audio !== currentAudio) {
+        audio.pause();
+      }
+    });
   }
 }
--- src/app/store/data-store.service.ts
+++ src/app/store/data-store.service.ts
@@ -7,6 +7,9 @@ import {BehaviorSubject, Observable} from "rxjs";
 export class DataStoreService {
   private curPart: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   private curAudio: BehaviorSubject<HTMLAudioElement | undefined> = new BehaviorSubject<HTMLAudioElement | undefined>(undefined);
+  private audios: BehaviorSubject<any[]> = new BehaviorSubject<any[]>([]); // Хранение аудиофайлов
+  private loading: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false); // Статус загрузки
+
 
   constructor() {
   }
@@ -25,5 +28,21 @@ export class DataStoreService {
 
   getCurAudio(): Observable<HTMLAudioElement | undefined> {
     return this.curAudio.asObservable();
+  }
+
+  setAudios(data: any[]): void {
+    this.audios.next(data);
+  }
+
+  getAudios(): Observable<any[]> {
+    return this.audios.asObservable();
+  }
+
+  setLoading(status: boolean): void {
+    this.loading.next(status);
+  }
+
+  getLoading(): Observable<boolean> {
+    return this.loading.asObservable();
   }
 }
