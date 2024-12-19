83116dbb021e79ce8a8ff1268a61e987c1a58c7a
StasRivkin <rsl14591@gmail.com>
2024-12-19 12:00:39 +0200

    19/12/24 SW repaired

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,10 +14,33 @@ export class AppComponent {
       this.swUpdate.versionUpdates.subscribe(event => {
         if (event.type === 'VERSION_READY') {
           if (confirm('A new version of the app is available. Would you like to load the new version?')) {
-            window.location.reload();
+            this.clearCache().then(() => {
+              window.location.reload();
+            });
           }
         }
       });
+      this.checkForUpdates();
+    }
+  }
+
+  checkForUpdates() {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.checkForUpdate().then(() => {
+        console.log('Update check complete');
+      }).catch(err => {
+        console.log('Update check failed:', err);
+      });
+    }
+  }
+
+  async clearCache() {
+    if ('caches' in window) {
+      const cacheNames = await caches.keys();
+      for (const cacheName of cacheNames) {
+        await caches.delete(cacheName);
+      }
+      console.log('Cache cleared');
     }
   }
 }
