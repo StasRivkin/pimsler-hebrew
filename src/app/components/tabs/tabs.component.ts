5da3c9f2285fb22610cfcb6ef702bedcf1e2925f
StasRivkin <rsl14591@gmail.com>
2024-12-22 10:55:10 +0200

    22/12/24 tabs scrolling

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -1,5 +1,7 @@
-import {Component,  OnInit} from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import {DataStoreService} from "../../store/data-store.service";
+import {MatTabGroup} from "@angular/material/tabs";
+import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
 
 @Component({
   selector: 'app-tabs',
@@ -16,10 +18,17 @@ export class TabsComponent implements OnInit {
   ngOnInit(): void {
     this.store.setCurPart(1);
     this.store.getCurPart().subscribe(data => {
+      const tabHeaderList = document.querySelector('.mat-mdc-tab-list');
+      if (tabHeaderList) {
+        (tabHeaderList as HTMLElement).style.overflowX = 'auto';
+        (tabHeaderList as HTMLElement).style.transform = 'unset';
+        tabHeaderList.classList.add('hide-scrollbar');
+      }
     });
   }
 
   async onTabChange(event: any) {
+    console.log("onTabChange", event);
     const selectedIndex = event.index;
     this.store.setCurPart(selectedIndex + 1);
     this.store.setCurAudio(null);
