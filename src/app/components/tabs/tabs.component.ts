9bfb1ec2a3afe6e3adada475f6bb6e736a21c611
StasRivkin <rsl14591@gmail.com>
2024-12-22 11:01:36 +0200

    22/12/24 tabs scrolling new

--- src/app/components/tabs/tabs.component.ts
+++ src/app/components/tabs/tabs.component.ts
@@ -17,18 +17,17 @@ export class TabsComponent implements OnInit {
 
   ngOnInit(): void {
     this.store.setCurPart(1);
-    // this.store.getCurPart().subscribe(data => {
-    //   const tabHeaderList = document.querySelector('.mat-mdc-tab-list');
-    //   if (tabHeaderList) {
-    //     (tabHeaderList as HTMLElement).style.overflowX = 'auto';
-    //     (tabHeaderList as HTMLElement).style.transform = 'unset';
-    //     tabHeaderList.classList.add('hide-scrollbar');
-    //   }
-    // });
+    this.store.getCurPart().subscribe(data => {
+      const tabHeaderList = document.querySelector('.mat-mdc-tab-list');
+      if (tabHeaderList) {
+        (tabHeaderList as HTMLElement).style.overflowX = 'auto';
+        (tabHeaderList as HTMLElement).style.transform = 'unset';
+        tabHeaderList.classList.add('hide-scrollbar');
+      }
+    });
   }
 
   async onTabChange(event: any) {
-    console.log("onTabChange", event);
     const selectedIndex = event.index;
     this.store.setCurPart(selectedIndex + 1);
     this.store.setCurAudio(null);
