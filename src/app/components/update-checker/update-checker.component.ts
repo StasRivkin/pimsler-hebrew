4819fec8747dbdd6828b6da648227bac60252b69
StasRivkin <rsl14591@gmail.com>
2024-12-25 10:23:23 +0200

    25/12/24 updater repaired

--- src/app/components/update-checker/update-checker.component.ts
+++ src/app/components/update-checker/update-checker.component.ts
@@ -1,14 +1,13 @@
-import {Component, OnInit} from '@angular/core';
-import {SwUpdate} from '@angular/service-worker';
-import {MatSnackBar} from '@angular/material/snack-bar';
+import { Component, OnInit } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-update-checker',
   template: '',
 })
 export class UpdateCheckerComponent implements OnInit {
-  constructor(private updates: SwUpdate, private snackBar: MatSnackBar) {
-  }
+  constructor(private updates: SwUpdate, private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
     if (this.updates.isEnabled) {
@@ -18,7 +17,7 @@ export class UpdateCheckerComponent implements OnInit {
         if (event.type === 'VERSION_DETECTED') {
           // Если обнаружена новая версия, показываем уведомление
           this.promptUserToUpdate();
-        }else {
+        } else {
           console.log('No updates');
         }
       });
@@ -31,7 +30,7 @@ export class UpdateCheckerComponent implements OnInit {
     const snackBarRef = this.snackBar.open(
       'Доступна новая версия приложения. Обновить?',
       'Обновить',
-      {duration: undefined} // Уведомление будет висеть до выбора действия
+      { duration: undefined } // Уведомление будет висеть до выбора действия
     );
 
     snackBarRef.onAction().subscribe(() => {
@@ -41,11 +40,17 @@ export class UpdateCheckerComponent implements OnInit {
 
   private async activateUpdate(): Promise<void> {
     try {
+      console.log('Activating update...');
       await this.clearCache(); // Сбрасываем кеш
       console.log('Cache cleared successfully.');
       await this.updates.activateUpdate(); // Активируем обновление
-      console.log('Update activated. Reloading the page...');
-      document.location.reload(); // Перезагружаем страницу
+      console.log('Update activated.');
+
+      // Убедимся, что обновление было активировано
+      if (this.updates.isEnabled) {
+        console.log('Reloading page...');
+        document.location.reload(); // Перезагружаем страницу
+      }
     } catch (error) {
       console.error('Error during update activation:', error);
     }
